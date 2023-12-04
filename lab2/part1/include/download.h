7290c65c0d898ef4d82b364f5199e922b914024e
francis802 <franciscosccampos@gmail.com>
2023-12-04 18:41:30 +0000

    commit

--- lab2/part1/include/download.h
+++ lab2/part1/include/download.h
@@ -1,2 +1,11 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+
+#define SERVER_PORT 6000
+#define SERVER_ADDR "192.168.28.96"
--- lab2/part1/src/download.c
+++ lab2/part1/src/download.c
@@ -1,7 +1,74 @@
 #include "download.h"
 
-int main(int argc, char const *argv[])
-{
-    printf("%s\n", argv[1]);
+int main(int argc, char **argv) {
+
+    struct hostent *h;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <address to get IP address>\n", argv[0]);
+        exit(-1);
+    }
+
+/**
+ * The struct hostent (host entry) with its terms documented
+
+    struct hostent {
+        char *h_name;    // Official name of the host.
+        char **h_aliases;    // A NULL-terminated array of alternate names for the host.
+        int h_addrtype;    // The type of address being returned; usually AF_INET.
+        int h_length;    // The length of the address in bytes.
+        char **h_addr_list;    // A zero-terminated array of network addresses for the host.
+        // Host addresses are in Network Byte Order.
+    };
+
+    #define h_addr h_addr_list[0]	The first address in h_addr_list.
+*/
+    if ((h = gethostbyname(argv[1])) == NULL) {
+        herror("gethostbyname()");
+        exit(-1);
+    }
+
+    printf("Host name  : %s\n", h->h_name);
+    printf("IP Address : %s\n", inet_ntoa(*((struct in_addr *) h->h_addr)));
+    
+    if (argc > 1)
+        printf("**** No arguments needed. They will be ignored. Carrying ON.\n");
+    int sockfd;
+    struct sockaddr_in server_addr;
+    char buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
+    size_t bytes;
+
+
+    bzero((char *) &server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);    
+    server_addr.sin_port = htons(SERVER_PORT);        
+
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket()");
+        exit(-1);
+    }
+
+    if (connect(sockfd,
+                (struct sockaddr *) &server_addr,
+                sizeof(server_addr)) < 0) {
+        perror("connect()");
+        exit(-1);
+    }
+
+    bytes = write(sockfd, buf, strlen(buf));
+    if (bytes > 0)
+        printf("Bytes escritos %ld\n", bytes);
+    else {
+        perror("write()");
+        exit(-1);
+    }
+
+    if (close(sockfd)<0) {
+        perror("close()");
+        exit(-1);
+    }
+    
     return 0;
 }
