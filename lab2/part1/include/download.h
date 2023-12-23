d55fd6636d50f3ac1ddf3c4f256098c456f168e7
francis802 <franciscosccampos@gmail.com>
2023-12-23 20:42:34 +0000

    .h update

--- lab2/part1/include/download.h
+++ lab2/part1/include/download.h
@@ -1,3 +1,6 @@
+#ifndef _DOWNLOAD_H_
+#define _DOWNLOAD_H_
+
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -23,3 +26,32 @@ typedef enum {
     MULTI_LINE,
     END
 } ReadServerState;
+
+// Function that checks if a string contains an @ symbol.
+// Returns 1 if the string contains an @ symbol, 0 otherwise.
+int containsAtSymbol(const char *str);
+
+// Function that gets the file name from the URL path.
+void getFileName(const char *urlPath);
+
+// Function that parses the FTP URL with the following format: ftp://[<user>:<password>@]<host>/<url-path>
+// Returns 1 if the URL contains a user and password, 0 otherwise.
+int parseFTPURL(const char *url, struct FTPURL *ftpURL);
+
+// Function that creates a new socket.
+// Returns the socket file descriptor on success, -1 otherwise.
+int newSocket(char* ip, int port);
+
+// Function that reads the server response.
+// Returns 1 on success, -1 otherwise.
+int serverResponse(int sockfd, char* response);
+
+// Function that authenticates the user.
+// Returns 1 on success, -1 otherwise.
+int authentication(int sockfd, char* user, char* password);
+
+// Function that enters passive mode, and gets the IP address and port to download the file.
+// Returns the port on success, -1 otherwise.
+int passiveMode(int sockfd, char* ip);
+
+#endif // _DOWNLOAD_H_
